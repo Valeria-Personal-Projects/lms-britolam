88642eece091333572b44d9b70d3121bbc364be4
unknown <svaleria.gonzales@gmail.com>
2024-05-07 15:49:29 -0400

    analytics route added to the teacher's page

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Layout, Compass } from "lucide-react"
+import { Layout, Compass, List, BarChart } from "lucide-react"
 import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 
 const guestRoutes = [
     {
@@ -16,8 +17,23 @@ const guestRoutes = [
     },
 ]
 
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Cursos",
+        href: "/teacher/courses",
+    },
+    {
+        icon: BarChart,
+        label: "Analíticas",
+        href: "/teacher/analytics",
+    },
+]
+
 export const SidebarRoutes = () => {
-    const routes = guestRoutes;
+    const pathname = usePathname();
+    const isTeacherPage = pathname?.includes("/teacher");
+    const routes = isTeacherPage? teacherRoutes : guestRoutes;
 
     return (
         <div className="flex flex-col w-full"> 
