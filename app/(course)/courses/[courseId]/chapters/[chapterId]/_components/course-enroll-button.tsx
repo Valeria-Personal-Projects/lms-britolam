8ad0e7c00d5e8484d5916a7c9090e764a1b8f425
unknown <svaleria.gonzales@gmail.com>
2024-09-09 19:10:47 -0400

    course purchase user interface done

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formatPrice } from "@/lib/format";
+
+interface CourseEnrollButtonProps {
+    price: number;
+    courseId: string;
+}
+
+export const CourseEnrollButton = ({
+    price,
+    courseId,
+}: CourseEnrollButtonProps) => {
+    return (
+        <Button 
+            size="sm"
+            className="w-full md:w-auto"
+        >
+            Inscríbete por {formatPrice(price)}
+        </Button>
+    )
+}
